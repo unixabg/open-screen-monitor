af508ebbc5b72023deb87c38228f7451952f96f2
andrew <andrewcoursen@gmail.com>
2018-01-30 15:25:09 -0600

    Move the OAuth to use the redirect method vs the javascript method, since the access_token didn't always get passed correctly.

--- php/index.php
+++ php/index.php
@@ -9,7 +9,6 @@ $client_secret_file = '../../client_secret.json';
 $devices_file = $dataDir.'/devices.tsv';
 $permissions_file = $dataDir.'/permissions.tsv';
 
-
 //load variables
 if (file_exists($client_secret_file)) {
 	$client_secret = json_decode(file_get_contents($client_secret_file));
@@ -48,7 +47,7 @@ function checkToken($token) {
 	if ( $http_response_header[0] == "HTTP/1.0 200 OK") {
 		$data = json_decode($data);
 
-		if ($data->aud == $client_secret->web->client_id && $data->exp > time() && $data->iss == "accounts.google.com") {
+		if ($data->aud == $client_secret->web->client_id && $data->exp > time() && $data->iss == "https://accounts.google.com") {
 			if (count($permissions) > 0 && !isset($permissions[$data->email])) {
 				die("<h1>Credentials valid. No Permissions</h1>");
 			}
@@ -68,13 +67,26 @@ function checkToken($token) {
 }
 
 
+//there are multiple pages contained in this file
+//first are pages that simply redirect or output for ajax calls
+//these need to be here so they can modify the http headers and completely control their output
+
 if (isset($_GET['logout'])) {
 	session_destroy();
 	header('Location: ?googleLogout');
 	die();
-} elseif (isset($_POST['token'])) {
-	$token = json_decode($_POST['token']);
-	//user handed us a token, lets check it and if valid, sign them in
+} elseif (isset($_GET['code'])) {
+	$data = file_get_contents('https://www.googleapis.com/oauth2/v4/token', false, stream_context_create(array('http' =>array(
+		'method'=>'POST',
+		'content' => http_build_query(array(
+			'code' => $_GET['code'],
+			'client_id' => $client_secret->web->client_id,
+			'client_secret' => $client_secret->web->client_secret,
+			'redirect_uri'=> $client_secret->web->redirect_uris[0],
+			'grant_type'=>'authorization_code'
+		)),
+	))));
+	$token = json_decode($data);
 	if (!checkToken($token))
 		die("Error has occured validating token");
 
@@ -95,123 +107,103 @@ if (isset($_GET['logout'])) {
 		header('Location: ?');
 	}
 	die();
-} elseif (isset($_GET['syncdevices']) && isset($_SESSION['token']) && checkToken($_SESSION['token'])) {
-	$context = stream_context_create(array('http' =>array(
-		'method'=>'GET',
-		'header'=>'Authorization: Bearer '.$_SESSION['token']->access_token,
-	)));
-	$url = 'https://www.googleapis.com/admin/directory/v1/customer/my_customer/devices/chromeos?projection=full&maxResults=100';
-	$data = file_get_contents($url, false, $context);
-	if ($data !== false) {
-		$data = json_decode($data,true);
-		$devices = $data['chromeosdevices'];
-		while (isset($data['nextPageToken']) && $data['nextPageToken'] != '') {
-			$data = file_get_contents($url.'&pageToken='.urlencode($data['nextPageToken']), false, $context);
-			if ($data === false) return false;
-				$data = json_decode($data,true);
-			$devices = array_merge($devices,$data['chromeosdevices']);
-		}
-		$toSave = "";
-		foreach ($devices as $device) {
-			if ($device['status'] == 'ACTIVE') {
-				if ($toSave != '') $toSave .= "\n";
-				$toSave .= $device['deviceId']."\t".$device['orgUnitPath']."\t".$device['annotatedUser']."\t".$device['annotatedLocation']."\t".$device['annotatedAssetId'];
-			}
-		}
-		file_put_contents($devices_file,$toSave);
-		echo "<h1>Successfully synced devices</h1>";
+}
 
-		//if permissions file doesn't exist, go ahead and create it giving the current user admin privs
-		if (!file_exists($permissions_file))
-			file_put_contents($permissions_file,$_SESSION['email']."\tadmin");
-	} else {
-		echo "<h1>No access to chrome devices</h1>";
-	}
-	die();
-} elseif (isset($_SESSION['token']) && checkToken($_SESSION['token'])) {
-	//user is authenticated, show them their labs
-	?>
+//begin html pages
+?>
 <html>
 <head>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
-	<h1>Logged In</h1>
-	<a href="?logout">Logout</a>
-	<center><div style="width: 500px;">
-		Hello <?php echo htmlentities($_SESSION['name'].' ('.$_SESSION['email'].')');?>
-		<br />Here are your labs to choose from:
-		<ul style="text-align:left;">
-		<?php
-		$myPermissions = $permissions[$_SESSION['email']];
-		if (in_array('admin',$myPermissions)) {
-			//show all devices
-			foreach (array_keys($labs) as $lab) {
-				echo "<li><a href=\"?lab=".urlencode($lab)."\">".htmlentities($lab)."</a></li>";
-			}
-		} else {
-			//show just what they can access
-			foreach ($myPermissions as $permission) {
-				$directPermissions .= "<li><a href=\"?lab=".urlencode($permission)."\">".htmlentities($permission)."</a></li>";
-			}
-		}
+<h1 style="display:inline;">Open Screen Monitor |</h1> <a href="?">Home</a> <?php if (isset($_SESSION['token'])){?>| <a href="?logout">Logout</a><?php } ?>
+<hr />
+<?php
 
+if (isset($_SESSION['token']) && checkToken($_SESSION['token'])) {
+	//user is authenticated
+
+	if (isset($_GET['syncdevices']) ) {
+		//sync devices
+		$context = stream_context_create(array('http' =>array(
+			'method'=>'GET',
+			'header'=>'Authorization: Bearer '.$_SESSION['token']->access_token,
+		)));
+		$url = 'https://www.googleapis.com/admin/directory/v1/customer/my_customer/devices/chromeos?projection=full&maxResults=100';
+		$data = file_get_contents($url, false, $context);
+		if ($data !== false) {
+			$data = json_decode($data,true);
+			$devices = $data['chromeosdevices'];
+			while (isset($data['nextPageToken']) && $data['nextPageToken'] != '') {
+				$data = file_get_contents($url.'&pageToken='.urlencode($data['nextPageToken']), false, $context);
+				if ($data === false) return false;
+					$data = json_decode($data,true);
+				$devices = array_merge($devices,$data['chromeosdevices']);
+			}
+			$toSave = "";
+			foreach ($devices as $device) {
+				if ($device['status'] == 'ACTIVE') {
+					if ($toSave != '') $toSave .= "\n";
+					$toSave .= $device['deviceId']."\t".$device['orgUnitPath']."\t".$device['annotatedUser']."\t".$device['annotatedLocation']."\t".$device['annotatedAssetId'];
+				}
+			}
+			file_put_contents($devices_file,$toSave);
+			echo "<h1>Successfully synced devices</h1>";
+
+			//if permissions file doesn't exist, go ahead and create it giving the current user admin privs
+			if (!file_exists($permissions_file))
+				file_put_contents($permissions_file,$_SESSION['email']."\tadmin");
+		} else {
+			echo "<h1>No access to chrome devices</h1>";
+		}
+	} else {
+		//the user is at the home (show labs) screen
 		?>
-		</ul>
-		<h2>Admin Tools</h2>
-		<ul style="text-align:left;">
-			<li><a href="?syncdevices" target="_blank">Sync Devices</a></li>
-		</ul>
-	</div></center>
-</body>
-</html>
-	<?php
-	die();
+		<h2>Hello <?php echo htmlentities($_SESSION['name'].' ('.$_SESSION['email'].')');?></h2>
+		<div>
+			Here are the labs you have access to:
+			<ul style="text-align:left;">
+			<?php
+			$myPermissions = $permissions[$_SESSION['email']];
+			if (in_array('admin',$myPermissions)) {
+				//show all devices
+				foreach (array_keys($labs) as $lab) {
+					echo "<li><a href=\"?lab=".urlencode($lab)."\">".htmlentities($lab)."</a></li>";
+				}
+			} else {
+				//show just what they can access
+				foreach ($myPermissions as $permission) {
+					$directPermissions .= "<li><a href=\"?lab=".urlencode($permission)."\">".htmlentities($permission)."</a></li>";
+				}
+			}
+			?>
+			</ul>
+		</div>
+		<?php if (in_array('admin',$myPermissions) || count($permissions) == 0) { ?>
+		<div>
+			<h2>Admin Tools</h2>
+			<ul style="text-align:left;">
+				<li><a href="?syncdevices" target="_blank">Sync Devices</a></li>
+			</ul>
+		</div>
+		<?php
+		}
+	 }
 } else {
 	//user needs to login, show them the login screen
 	?>
-<html>
-<head>
-	<meta name="google-signin-client_id" content="<?php echo $client_secret->web->client_id;?>">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-</head>
-<body>
 	<h1>Login</h1>
 	<center>
 	<div id="myGoogleSignin"></div>
 	</center>
-	<script type="text/javascript">
-		var signOut = <?php echo isset($_GET['googleLogout']) ? 'true':'false'; ?>;
-		var onSuccess = function(googleUser) {
-			if (signOut) {
-				var auth2 = gapi.auth2.getAuthInstance();
-				auth2.signOut();
-				signOut = false;
-			} else {
-				console.log(googleUser.getAuthResponse());
-				var response = googleUser.getAuthResponse();
-
-				var form = $('<form />').attr('method','POST');
-				var input = $('<input>').attr('type','hidden').attr('name','token').val(JSON.stringify(response));
-				$(form).append($(input));
-				form.appendTo( document.body )
-				$(form).submit();
-			}
-		}
-		function onGoogleLoad() {
-			gapi.signin2.render('myGoogleSignin', {
-				'scope': 'profile email https://www.googleapis.com/auth/admin.directory.device.chromeos.readonly',
-				'width': 240,
-				'height': 50,
-				'longtitle': true,
-				'theme': 'dark',
-				'onsuccess': onSuccess,
-			});
-		};
-	</script>
-	<script src="https://apis.google.com/js/platform.js?onload=onGoogleLoad" async defer></script>
+	<?php
+	echo "<a href=\"https://accounts.google.com/o/oauth2/v2/auth?scope="
+		.urlencode("profile email https://www.googleapis.com/auth/admin.directory.device.chromeos.readonly")
+		."&response_type=code"
+		."&client_id=".$client_secret->web->client_id
+		."&redirect_uri=".urlencode($client_secret->web->redirect_uris[0])
+		."\"><img src=\"google_signin.png\" alt=\"Google Signin\" /></a>";
+}
+?>
 </body>
 </html>
-	<?php
-	die();
-}
