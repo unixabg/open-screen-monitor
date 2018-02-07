561aae786b16ccc9cf6d34acd0d009bbef068a61
Richard Nelson <unixabg@gmail.com>
2018-02-07 12:34:24 -0600

    Add style.css to php/index.php.

--- php/index.php
+++ php/index.php
@@ -160,6 +160,7 @@ if (isset($_GET['logout'])) {
 <html>
 <head>
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="./style.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
