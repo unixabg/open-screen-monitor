d90a2d09a6ab85e0dc1af39da4eda5ca9fdc1e1a
Andrew Coursen <AndrewCoursen@gmail.com>
2018-02-21 23:29:39 -0600

    Initial commit of php/filter.php.

---
+++ php/filter.php
@@ -0,0 +1,47 @@
+<?php
+$dataDir = '../../osm-data';
+if (!file_exists($dataDir)) die('Missing osm-data directory');
+$logDir = $dataDir.'/logs/';
+if (!file_exists($logDir)) mkdir($logDir,0755,true);
+
+$data = isset($_POST['data']) ? json_decode($_POST['data'],true) : array();
+if (isset($data['username']) && isset($data['domain']) && isset($data['deviceID']) && isset($data['url']) && $data['url'] != ''){
+	$data['username'] = preg_replace("/[^a-z0-9-_\.]/","",$data['username']);
+	if ($data['username'] == '') $data['username'] = 'unknown';
+
+	$data['domain'] = preg_replace("/[^a-z0-9-_\.]/","",$data['domain']);
+	if ($data['domain'] == '') $data['domain'] = 'unknown';
+
+	$data['deviceID'] = preg_replace("/[^a-z0-9-]/","",$data['deviceID']);
+	if ($data['deviceID'] == '') $data['deviceID'] = 'unknown';
+
+
+	$data['url'] = str_replace("\t","",$data['url']);
+	$data['url'] = str_replace("\n","",$data['url']);
+
+
+	//determine action
+	$action = 'ALLOW';
+	$redirectURL = '';
+	//TODO
+
+
+
+	//log it
+	$logentry = $action."\t".time()."\t".$data['url']."\n";
+	$logDir .= $data['username']."_".$data['domain']."/";
+	if (!file_exists($logDir)) mkdir($logDir,0755,true);
+	$logDir .= $data['deviceID']."/";
+	if (!file_exists($logDir)) mkdir($logDir,0755,true);
+	$logDir .= date('Y-m-d')."/";
+	if (!file_exists($logDir)) mkdir($logDir,0755,true);
+	$logFile = $logDir.str_replace(".",'-',$_SERVER['REMOTE_ADDR']).".tsv";
+	if (!file_exists($logFile)) touch($logFile);
+	file_put_contents($logFile, $logentry, FILE_APPEND | LOCK_EX);
+
+
+	//send it back
+	die($action.($redirectURL != '' ? "\n".$redirectURL : ""));
+}
+//if we get here, there has been a problem
+die("Error in request");
