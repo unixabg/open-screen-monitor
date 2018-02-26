14d0a682bca68ba4f2191b4e58abf799bd3ca6bd
Richard Nelson <unixabg@gmail.com>
2018-02-25 19:16:56 -0600

    Adjust version information in osm-data/defaults.php.

--- osm-data/defaults.php
+++ osm-data/defaults.php
@@ -2,11 +2,11 @@
 //global default settings
 
 /* *************************** Required variables ***************************
-As of version 0.2.0.2 variables below this line are required for OSM to operate correctly
+As of version 0.2.0.1 variables below this line are required for OSM to operate correctly
 */
 
 //set system wide version for php scripts
-$_gVersion='0.2.0.1';
+$_gVersion='0.2.0.2';
 
 //set the default time chrome will wait between phone home attempst to the upload script
 $_gUploadRefreshTime=9000;
