7b1546ef389fe16403498bec766a5771ea7f7401
Richard Nelson <unixabg@gmail.com>
2018-10-25 13:43:29 -0500

    Add version at the top in php/index.php.

--- php/index.php
+++ php/index.php
@@ -220,7 +220,7 @@ if (isset($_GET['logout'])) {
 	<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 </head>
 <body>
-<h1 style="display:inline;">Open Screen Monitor |</h1> <a href="?">Home</a> <?php if (isset($_SESSION['token'])){?>| <a href="?logout">Logout</a><?php } ?>
+<h1 style="display:inline;">Open Screen Monitor |</h1> <a href="?">Home</a> <?php if (isset($_SESSION['token'])){?>| <a href="?logout">Logout</a><?php } echo "<div style=\"display:inline; float:right; padding-top:20px; padding-right:10px;\">Version ".$_config['version']."</div>"; ?>
 <hr />
 <?php
 
@@ -431,7 +431,6 @@ if (isset($_SESSION['token']) && checkToken($_SESSION['token'])) {
 		."&redirect_uri=".urlencode($client_secret->web->redirect_uris[0])
 		."\"><img src=\"google_signin.png\" alt=\"Google Signin\" /></a><br />";
 }
-echo "<br />Version ".$_config['version'];
 ?>
 </body>
 </html>
