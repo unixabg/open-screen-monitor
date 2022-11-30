dcda8b064ca0b11242363b866d70974d08f8f73f
Richard Nelson <unixabg@gmail.com>
2022-11-30 09:56:12 -0600

    Fix console log and scope issue on logging deviceID in chrome-extension/manifest_version-3/background.js.

--- chrome-extension/manifest_version-3/background.js
+++ chrome-extension/manifest_version-3/background.js
@@ -26,8 +26,10 @@ chrome.storage.local.get(null).then(data => {
 
 	//get deviceID
 	if (typeof(chrome["enterprise"]) !== "undefined") {
-		chrome.enterprise.deviceAttributes.getDirectoryDeviceId(function(tempDevID) {chrome.storage.local.set({deviceID: tempDevID});});
-		console.log('Managed device with DeviceIdOfTheDirectoryAPI: ', tempDevId);
+		chrome.enterprise.deviceAttributes.getDirectoryDeviceId(function(tempDevID) {
+			chrome.storage.local.set({deviceID: tempDevID});
+			console.log('Managed device with DeviceIdOfTheDirectoryAPI: ', tempDevId);
+		});
 	} else {
 		console.log("Info: not a managed device.");
 	}
