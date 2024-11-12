8702c93b882ac4ac18617f1bd299fbd3ef925807
Andrew Coursen <AndrewCoursen@gmail.com>
2024-11-12 14:57:24 -0600

    Fixed an issue where teachers can't view the filter log

--- php/Route/Monitor/Filterlog.php
+++ php/Route/Monitor/Filterlog.php
@@ -121,19 +121,23 @@ class Filterlog extends \OSM\Tools\Route {
 
 			if (!$_SESSION['admin']){
 				$subwhere = [];
-				foreach(array_keys($_SESSION['allowedclients']) as $i => $clientid){
-					$subwhere[] = ':client'.$i;
-					$bindings[':client'.$i] = $clientid;
-				}
-				$subwhere = '('.implode(',',$subwhere).')';
+				$i = 0;
+				foreach(['devices','users'] as $type){
+					if (!isset($_SESSION['clients'][$type])){continue;}
 
-				if (\OSM\Tools\Config::get('mode') == 'user'){
-					$where[] = 'username IN '.$subwhere;
-				} elseif (\OSM\Tools\Config::get('mode') == 'device'){
-					$where[] = 'deviceid IN '.$subwhere;
-				} else {
-					echo "Error finding Mode";
-					return;
+					$subwhere = [];
+					foreach($_SESSION['clients'][$type] as $clientID => $clientName){
+						$subwhere[] = ':client'.$i;
+						$bindings[':client'.$i] = $clientID;
+						$i++;
+					}
+					$subwhere = '('.implode(',',$subwhere).')';
+
+					if ($type == 'devices'){
+						$where[] = 'deviceid IN '.$subwhere;
+					} elseif ($type == 'users'){
+						$where[] = 'username IN '.$subwhere;
+					}
 				}
 			}
 
@@ -200,7 +204,7 @@ class Filterlog extends \OSM\Tools\Route {
 		echo '<br />Device:<br /><select name="device">';
 			echo '<option></option>';
 			foreach($deviceNames as $deviceid => $nicename){
-				if (!$_SESSION['admin'] && !in_array($row['deviceid'], $_SESSION['allowedclients'])){continue;}
+				if (!$_SESSION['admin'] && !isset($_SESSION['clients']['devices'][$row['deviceid']])){continue;}
 				echo '<option value="'.htmlentities($deviceid).'" '.($device == $deviceid ? 'selected' : '').'>'.htmlentities($nicename).'</option>';
 			}
 			echo '</select>';
