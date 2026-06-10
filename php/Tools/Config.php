2adc44cbc3fc77bdaf4b6f22fadcb76885e6f9fd
Richard Nelson <unixabg@gmail.com>
2026-06-10 09:59:14 -0500

    Bump for merge to master v0.4.0.5.

--- php/Tools/Config.php
+++ php/Tools/Config.php
@@ -9,7 +9,7 @@ class Config {
 			self::$config = [];
 
 			//set system wide version for php scripts
-			self::$config['version']='0.4.0.5-next';
+			self::$config['version']='0.4.0.5';
 
 			//set the default time chrome will wait between phone home attempst to the upload script
 			self::$config['uploadRefreshTime']=9000;
