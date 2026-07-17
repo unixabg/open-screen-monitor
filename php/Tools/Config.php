d18d5cbe22000170b2120357db4981291258fda1
Richard Nelson <unixabg@gmail.com>
2026-07-17 17:28:47 -0500

    Bump version in next for development.

--- php/Tools/Config.php
+++ php/Tools/Config.php
@@ -9,7 +9,7 @@ class Config {
 			self::$config = [];
 
 			//set system wide version for php scripts
-			self::$config['version']='0.4.0.8';
+			self::$config['version']='0.4.0.9-next';
 
 			//set the default time chrome will wait between phone home attempst to the upload script
 			self::$config['uploadRefreshTime']=9000;
